1194fdfdf199dbbc8252726159fe68a880e8927d
Manav Misra <manavm@visionwebsoft.com>
2025-11-17 20:00:37 -0600

    Add fake database

---
+++ src/db.js
@@ -0,0 +1,19 @@
+export default [
+  {
+    id: 1,
+    name: "Manav Misra",
+  },
+  {
+    id: 2,
+    name: "John Doe",
+  },
+  { id: 3, name: "Jane Smith" },
+  {
+    id: 4,
+    name: "Alice Johnson",
+  },
+  {
+    id: 5,
+    name: "Bob Brown",
+  },
+];

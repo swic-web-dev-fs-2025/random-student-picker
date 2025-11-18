fdefbedf2cb0515ffbf8168c92d88007d9855eed
Manav Misra <manavm@visionwebsoft.com>
2025-11-17 20:20:52 -0600

    Render random student on the screen

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
 import "./style.css";
 
-document.querySelector("#app").innerHTML = `
-  <h1 class="text-3xl font-bold underline">
-    Hello Vite!
-  </h1>
-`;
+import data from "./db";
+
+const button = document.querySelector("button");
+const nameDisplay = document.querySelector("p");
+
+button.addEventListener("click", () => {
+  nameDisplay.textContent =
+    data[
+      getRandomIntInclusive(
+        0,
+
+        // Subtract 1 to account for zero-based indexing
+        data.length - 1,
+      )
+    ].name;
+});
+
+function getRandomIntInclusive(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
+}

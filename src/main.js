c6656357abc10f4024d2ed64ea6e1f9c4663d48f
Manav Misra <manavm@visionwebsoft.com>
2025-11-17 20:39:46 -0600

    feat: add dark/light mode switch

--- src/main.js
+++ src/main.js
@@ -2,8 +2,11 @@ import "./style.css";
 
 import data from "./db";
 
+const body = document.querySelector("body");
 const button = document.querySelector("button");
 const nameDisplay = document.querySelector("p");
+const darkModeRadio = document.getElementById("dark");
+const lightModeRadio = document.getElementById("light");
 
 button.addEventListener("click", () => {
   nameDisplay.textContent =
@@ -15,6 +18,16 @@ button.addEventListener("click", () => {
         data.length - 1,
       )
     ].name;
+});
+
+darkModeRadio.addEventListener("change", () => {
+  body.classList.remove("bg-gray-100");
+  body.classList.add("bg-gray-800", "text-white");
+});
+
+lightModeRadio.addEventListener("change", () => {
+  body.classList.remove("bg-gray-800", "text-white");
+  body.classList.add("bg-gray-100");
 });
 
 function getRandomIntInclusive(min, max) {
